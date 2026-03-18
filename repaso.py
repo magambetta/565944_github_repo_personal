343091cb6007716709571680f5198a22425fd993
magambetta <magambetta@uade.edu.ar>
2026-03-18 19:24:47 -0300

    primer commmit

---
+++ repaso.py
@@ -0,0 +1,22 @@
+# COmentarios en linea
+"""
+1
+2
+3
+
+"""
+
+print("calculadora en python")
+
+# inicializar variables
+numero1 = 4
+numero2 = 6
+
+suma = numero1 + numero2
+
+#sumar los numeros
+
+print("el resultado de la suma es: {suma}")
+
+
+
